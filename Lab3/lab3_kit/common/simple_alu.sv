77c452e54a0e55145e80648df4b68c705aa495c5
Michael Zhao <mweiyuzhao@gmail.com>
2018-02-03 18:00:04 -0500

    simplified simple alu of/uf logic

--- Lab3/lab3_kit/common/simple_alu.sv
+++ Lab3/lab3_kit/common/simple_alu.sv
@@ -16,25 +16,15 @@ module simple_alu #
   localparam ADD = 0;
   localparam SUB = 1;
 
+  assign o_overflow = (~i_salu_a[WIDTH-1]) & (~i_salu_b[WIDTH-1]) & (i_salu_sel == ADD);
+  assign o_underflow = (i_salu_a[WIDTH-1]) & (i_salu_b[WIDTH-1]) & (i_salu_sel == SUB);
+
   always_comb begin
     case (i_salu_sel)
       ADD: o_salu_out = i_salu_a + i_salu_b;
       SUB: o_salu_out = i_salu_a - i_salu_b;
       default: o_salu_out = {WIDTH{1'b0}};
     endcase
-  end
-
-  always_comb begin
-
-    o_overflow = 1'b0;
-    o_underflow = 1'b0;
-
-    if (~i_salu_a[WIDTH-1] & ~i_salu_b[WIDTH-1] & i_salu_sel == ADD)
-      o_overflow = 1'b1;
-
-    if (i_salu_a[WIDTH-1] & i_salu_b[WIDTH-1] & i_salu_sel == SUB)
-      o_underflow = 1'b1;
-
   end
 
 endmodule
