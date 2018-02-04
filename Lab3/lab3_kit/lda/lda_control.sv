e6ebd03e6e81900e350f0d42f50eca242bbd4e02
Co Ivan <coivan@WIN.ecf.utoronto.ca>
2018-02-03 22:35:45 -0500

    Finished line drawing algorithm

--- Lab3/lab3_kit/lda/lda_control.sv
+++ Lab3/lab3_kit/lda/lda_control.sv
@@ -1,24 +1,95 @@
 module lda_control
 (
-  input clk,
-  input reset,
+  input i_clk,
+  input i_reset,
   
+  // in from UI
   input i_start,
   
+  // out to UI
   output o_done,
   
+  // out to VGA
   output o_plot,
   
-  input i_x0_gt_x1,
+  // in from data
+  input i_keep_drawing,
   
-  output o_ld_constants,
-  output o_ld_error,
-  output o_ld_ystep,
-  output o_ld_xstep,
-  output o_ld_y,
-  output o_ld_x
+  // out to data
+  output o_ld_initial,
+  output o_update_error,
+  output o_update_y,
+  output o_update_x,
+  output [1:0] o_mux_cmp0,
+  output o_mux_alu0,
+  output o_mux_alu1
 );
 
+// States
+enum int unsigned
+{
+	S_WAIT,
+  S_INIT,
+  S_LOOP_CHECK,
+  S_DRAW
+} state, nextstate;
 
+// State regs
+always_ff @ (posedge i_clk or posedge i_reset) begin
+	if (i_reset) state <= S_WAIT;
+	else state <= nextstate;
+end
+
+// State table
+always_comb begin
+	nextstate = state;
+	o_done = 1'b0;
+  o_plot = 1'b0;
+  
+  o_ld_initial = 1'b0;
+  o_update_error = 1'b0;
+  o_update_y = 1'b0;
+  o_update_x = 1'b0;
+  
+  o_mux_cmp0 = 2'd0;
+  
+  o_mux_alu0 = 1'b0;
+  o_mux_alu1 = 1'b0;
+	
+	case (state)
+		S_WAIT: begin
+			o_done = 1'b0;
+			if (i_start) nextstate = S_INIT;
+		end
+    S_INIT: begin
+      o_ld_initial = 1'b1;
+      
+      o_mux_cmp0 = 2'd1;
+      
+      o_mux_alu0 = 1'b0;
+      o_mux_alu1 = 1'b0;
+      
+      nextstate = S_LOOP_CHECK;
+    end
+    S_LOOP_CHECK: begin
+      o_mux_cmp0 = 2'd1;
+      nextstate = i_keep_drawing ? S_DRAW : S_WAIT;
+    end
+    S_DRAW: begin
+      o_plot = 1'b1;
+      
+      o_update_error = 1'b1;
+      o_update_y = 1'b1;
+      o_update_x = 1'b1;
+      
+      o_mux_cmp0 = 2'd2;
+      
+      o_mux_alu0 = 1'b1;
+      o_mux_alu1 = 1'b1;
+      
+      nextstate = S_LOOP_CHECK;
+    end
+	endcase
+end
 
 endmodule
--- Lab3/lab3_kit/lda/lda_datapath.sv
+++ Lab3/lab3_kit/lda/lda_datapath.sv
@@ -3,39 +3,45 @@ module lda_datapath
   input i_clk,
   input i_reset,
   
+  // in from UI
   input [8:0] i_x0,
   input [8:0] i_x1,
   input [7:0] i_y0,
   input [7:0] i_y1,
-  input i_color,
+  input [2:0] i_color,
   
-  output o_x,
-  output o_y,
-  output o_color,
-
-  input i_ld_constants,
-  input i_ld_error,
-  input i_ld_ystep,
-  input i_ld_xstep,
-  input i_ld_y,
-  input i_ld_x,
+  // out to VGA
+  output [8:0] o_x,
+  output [7:0] o_y,
+  output [2:0] o_color,
   
-  output logic o_x0_gt_x1
+  // in from control
+  input i_ld_initial,
+  input i_update_error,
+  input i_update_y,
+  input i_update_x,
+  input [1:0] i_mux_cmp0,
+  input i_mux_alu0,
+  input i_mux_alu1,
+  
+  // out to control
+  output o_keep_drawing
 );
 
 logic steep;
-logic dx;
-logic dy;
-
-logic [1:0] error;
+logic [8:0] dx;
+logic [7:0] dy;
+logic x0_gt_x1;
 logic [1:0] ystep;
 logic [1:0] xstep;
-logic y;
-logic x;
+
+logic [8:0] error;
+logic [8:0] x;
+logic [7:0] y;
 
 logic [3:0][8:0] alu_a;
 logic [3:0][8:0] alu_b;
-logic [3:0] alu_sel;
+logic [3:0]      alu_sel;
 logic [3:0][8:0] alu_out;
 
 genvar i;
@@ -54,9 +60,9 @@ endgenerate
 
 logic [2:0][8:0] cmp_a;
 logic [2:0][8:0] cmp_b;
-logic [2:0] cmp_gt;
-logic [2:0] cmp_lt;
-logic [2:0] cmp_eq;
+logic [2:0]      cmp_gt;
+logic [2:0]      cmp_lt;
+logic [2:0]      cmp_eq;
 
 generate
   for (i = 0; i < 3; i++) begin : comparator
@@ -73,96 +79,167 @@ generate
   end
 endgenerate
 
+assign o_keep_drawing = (x0_gt_x1 ? cmp_gt[0] : cmp_lt[0] | cmp_eq[0]);
+
+assign o_x = steep ? y : x;
+assign o_y = steep ? x : y;
+assign o_color = i_color;
+
 always_comb begin
-  cmp_a[0] = i_y1;
-  cmp_b[0] = i_y0;
-  
-  case (cmp_gt[0])
+  // Comparators
+  // cmp 0
+  case (i_mux_cmp0)
+    // S_INIT
     0: begin
-      alu_a[0] = i_y0;
-      alu_b[0] = i_y1;
+      // y1 > y0
+      cmp_a[0] = i_y1;
+      cmp_b[0] = i_y0;
     end
+    // S_LOOP_CHECK
     1: begin
-      alu_a[0] = i_y1;
-      alu_b[0] = i_y0;
+      // compare x, x1
+      cmp_a[0] = x;
+      cmp_b[0] = i_x1;
+    end
+    // S_DRAW
+    2: begin
+      // error - dy < 0
+      cmp_a[0] = alu_out[0];
+      cmp_b[0] = 0;
+    end
+    default: begin
+      cmp_a[0] = 0;
+      cmp_b[0] = 0;
     end
   endcase
-  
+    
+  //cmp 1
+  // S_INIT
+  // x1 > x0
   cmp_a[1] = i_x1;
   cmp_b[1] = i_x0;
   
-  case (cmp_gt[1])
+  // cmp 2
+  // S_INIT
+  // abs(y1 - y0) - abs(x1 - x0)
+  cmp_a[2] = alu_out[0];
+  cmp_b[2] = alu_out[1];
+  
+  // ALUs
+  // alu 0
+  case (i_mux_alu0)
+    // S_INIT
     0: begin
-      alu_a[1] = i_x0;
-      alu_b[1] = i_x1;
+      // abs(y1 - y0)
+      // y1 > y0 ? y1 - y0 : y0 - y1
+      alu_sel[0] = 1;
+      alu_a[0] = cmp_gt[0] ? i_y1 : i_y0;
+      alu_b[0] = cmp_gt[0] ? i_y0 : i_y1;
     end
+    // S_DRAW
     1: begin
-      alu_a[1] = i_x1;
-      alu_b[1] = i_x0;
+      // error - dy
+      alu_a[0] = error;
+      alu_b[0] = dy;
+      alu_sel[0] = 1;
+    end
+    default: begin
+      alu_a[0] = 0;
+      alu_b[0] = 0;
+      alu_sel[0] = 0;
     end
   endcase
   
-  cmp_a[2] = alu_out[0];
-  cmp_b[2] = alu_out[1];
+  // alu 1
+  case (i_mux_alu1)
+    // S_INIT
+    0: begin
+      // abs(x1 - x0)
+      // x1 > x0 ? x1 - x0 : x0 - x1
+      alu_sel[1] = 1;
+      alu_a[1] = cmp_gt[0] ? i_x1 : i_x0;
+      alu_b[1] = cmp_gt[0] ? i_x0 : i_x1;
+      end
+    // S_DRAW
+    1: begin
+      // error - dy + dx
+      alu_a[1] = alu_out[0];
+      alu_b[1] = dx;
+      alu_sel[1] = 0;
+    end
+    default: begin
+      alu_a[1] = 0;
+      alu_b[1] = 0;
+      alu_sel[1] = 0;
+    end
+  endcase
+  
+  // alu 2
+  // S_DRAW
+  // y + ystep
+  alu_a[2] = y;
+  alu_b[2] = ystep;
+  alu_sel[2] = 0;
+  
+  // alu 3
+  // S_DRAW
+  // x + xstep
+  alu_a[3] = x;
+  alu_b[3] = xstep;
+  alu_sel[3] = 0;
 end
 
 always_ff @ (posedge i_clk or posedge i_reset) begin
-  // steep, dx, dy
+  // steep, dx, dy, x0_gt_x1, xstep, ystep
   if (i_reset) begin
     steep <= 0;
     dx <= 0;
     dy <= 0;
-    
-    o_x0_gt_x1 <= 0;
+    x0_gt_x1 <= 0;
+    ystep <= 0;
+    xstep <= 0;
   end
-  else if (i_ld_constants) begin
+  else if (i_ld_initial) begin
     steep <= cmp_gt[2];
     dx <= cmp_gt[2] ? alu_out[0] : alu_out[1];
     dy <= cmp_gt[2] ? alu_out[1] : alu_out[0];
-    
-    o_x0_gt_x1 <= cmp_gt[2] ? ~cmp_gt[0] : ~cmp_gt[1];
+    x0_gt_x1 <= cmp_gt[2] ? ~cmp_gt[0] : ~cmp_gt[1];
+    ystep <= (cmp_gt[2] ? cmp_gt[1] : cmp_gt[0]) ? 2'd1 : -2'd1;
+    xstep <= (cmp_gt[2] ? cmp_gt[0] : cmp_gt[1]) ? -2'd1 : 2'd1; 
   end
   
   // error
   if (i_reset) begin
     error <= 0;
   end
-  else if (i_ld_constants) begin
+  else if (i_ld_initial) begin
     error <= (cmp_gt[2] ? alu_out[0] : alu_out[1]) >> 1;
   end
-  
-  // ystep
-  if (i_reset) begin
-    ystep <= 0;
-  end
-  else if (i_ld_constants) begin
-    ystep <= (cmp_gt[2] ? cmp_gt[1] : cmp_gt[0]) ? 1 : -1;
-  end
-  
-  // xstep
-  if (i_reset) begin
-    xstep <= 0;
-  end
-  else if (i_ld_constants) begin
-    xstep <= (cmp_gt[2] ? cmp_gt[0] : cmp_gt[1]) ? -1 : 1; 
+  else if (i_update_error) begin
+    error <= cmp_lt[0] ? alu_out[1] : alu_out[0];
   end
   
   // y
   if (i_reset) begin
     y <= 0;
   end
-  else if (i_ld_constants) begin
+  else if (i_ld_initial) begin
     y <= cmp_gt[2] ? i_x0 : i_y0;
+  end
+  else if (i_update_y) begin
+    y = cmp_lt[0] ? alu_out[2] : y;
   end
   
   // x
   if (i_reset) begin
     x <= 0;
   end
-  else if (i_ld_constants) begin
-    y <= cmp_gt[2] ? i_y0 : i_x0;
+  else if (i_ld_initial) begin
+    x <= cmp_gt[2] ? i_y0 : i_x0;
   end
-  
+  else if (i_update_x) begin
+    x <= alu_out[3];
+  end
 end
 
 endmodule
